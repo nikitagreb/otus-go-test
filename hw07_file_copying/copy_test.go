7043b277fe15176433636d6ed41d6203fee2e869
Никита Гребенников <nvgrebennikov@sbercloud.ru>
2022-09-18 16:48:57 +0300

    HW7 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,102 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	cases := []struct {
+		testName string
+		from     string
+		offset   int64
+		limit    int64
+	}{
+		{"out offset0 limit0", "out_offset0_limit0.txt", 0, 0},
+		{"out offset0 limit10", "out_offset0_limit10.txt", 0, 10},
+		{"out offset0 limit1000", "out_offset0_limit1000.txt", 0, 1000},
+		{"out offset0 limit10000", "out_offset0_limit10000.txt", 0, 10000},
+	}
+
+	for _, tcase := range cases {
+		t.Run(tcase.testName, func(t *testing.T) {
+			from := "testdata/" + tcase.from
+
+			f, err := os.CreateTemp("", "test_copy_")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer os.Remove(f.Name())
+
+			errCopy := Copy(from, f.Name(), tcase.offset, tcase.limit)
+			require.NoError(t, errCopy)
+
+			bCopy, err := os.ReadFile(f.Name())
+			if err != nil {
+				log.Fatal(err)
+			}
+			bOriginal, err := os.ReadFile(from)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			require.Equal(t, string(bOriginal), string(bCopy))
+		})
+	}
+
+	t.Run("out offset100 limit10000", func(t *testing.T) {
+		from := "testdata/" + "out_offset100_limit1000.txt"
+
+		f, err := os.CreateTemp("", "test_copy_")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer os.Remove(f.Name())
+
+		errCopy := Copy(from, f.Name(), 100, 1000)
+		require.NoError(t, errCopy)
+
+		bCopy, err := os.ReadFile(f.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fOriginal, err := os.Open(from)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = fOriginal.Seek(100, io.SeekStart)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		bOriginal := make([]byte, 900)
+		_, err = fOriginal.Read(bOriginal)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		require.Equal(t, string(bOriginal), string(bCopy))
+	})
+
+	t.Run("out offset6000 limit1000", func(t *testing.T) {
+		from := "testdata/" + "out_offset6000_limit1000.txt"
+
+		f, err := os.CreateTemp("", "test_copy_")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer os.Remove(f.Name())
+
+		errCopy := Copy(from, f.Name(), 6000, 1000)
+		require.Errorf(t, errCopy, "offset exceeds file size")
+	})
 }
--- hw07_file_copying/go.mod
+++ hw07_file_copying/go.mod
@@ -1,3 +1,10 @@
-module github.com/fixme_my_friend/hw07_file_copying
+module github.com/nikitagreb/otus-go-test/hw07_file_copying
 
 go 1.16
+
+require (
+	github.com/rivo/uniseg v0.3.4 // indirect
+	github.com/schollz/progressbar/v3 v3.10.1
+	github.com/stretchr/testify v1.3.0
+	golang.org/x/sys v0.0.0-20220908150016-7ac13a9a928d // indirect
+)
